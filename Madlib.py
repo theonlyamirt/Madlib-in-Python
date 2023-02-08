57f4b914f76b02b0c08ad54236ecc303090d7427
Amir TAJIK <85240771+theonlyamirt@users.noreply.github.com>
2023-02-08 17:47:01 +0100

    Add files via upload
    In the traditional Madlib, you would have a bunch of blanks in a paragraph, and you would have somebody fill out those blanks and then read a paragraph out loud with the word that they chose in those blanks.
    So, we’re going to recreate this project in python using string concatenation

---
+++ Madlib.py
@@ -0,0 +1,9 @@
+adj=input(" adjectif ")
+verb1=input(" verbe1 ")
+verb2=input(" verb2 ")
+famousperson=input(" famousperson")
+
+madlib=f"Computer programming is so {adj} it make me \
+so excited all the time because I love to {verb1}.Stay hydrated and {verb2}.Like \
+You are {famousperson}"
+print(madlib)
